37b80f33a9d2945335db94b4f4d7bd29bd220814
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-09-20 20:47:04 +0200

    last changes to ex1

--- exercise1/ex1_support/ex1.s
+++ exercise1/ex1_support/ex1.s
@@ -159,8 +159,21 @@ _enable_interrupts:
 
 	str r3, [r1, #0]
 
+	// Setting initial value in output registers
+
+	mov r4, #1
+
+	lsl r4, r4, #8
+
+	mvn r4, r4 
+
+	str r4, [r2, #GPIO_DOUT]
+
     //wait for instructions
 	wfi
+
+
+
 
 
 gpio_pc_model:
@@ -210,9 +223,66 @@ gpio_handler:
     ldr r3, gpio_base
     str r7, [r3, #GPIO_IFC]	  
     ldr r1, [r5, #GPIO_DIN]
-    lsl r1, r1, #8
-    str r1, [r2, #GPIO_DOUT]
-    bx lr	
+
+    mov r6, #0x10 // Mask for detecting button press on button 5
+
+    mov r7, #0x40 // Mask for detecting button press on button 7
+    
+    and r3, r1, r6 
+
+    cbz r3, _handle_button_5 // Button 5 is pressed if bit 4 is 0
+
+_return_button_5:
+
+    and r3, r1, r7
+
+    cbz r3, _handle_button_7 // Button 7 is pressed if bit 6 is 0
+
+_return_button_7:
+    mov r4, r4     
+
+    str r4, [r2, #GPIO_DOUT]
+    bx lr
+
+_handle_button_5:
+	
+	lsr r4, r4, #8 // Moving output vector into the first 8 bit of the register
+
+  mov r8, #0xff // Mask for extracting only the bits of the output vector
+
+	and r4, r4, r8 // Does extraction
+
+	mov r8, #0x01 // Mask for extracting bit 0 in the output vector
+
+	and r8, r8, r4 // Does extraction
+
+	lsl r4, r4, #7 // Moving the output vector back to the output window shifted one bit to the right 
+
+	lsl r8, r8, #15 // Movong the first bit of the output vector to last position 
+
+  	orr r4, r4, r8 // Adding the last bit to the output word
+
+	B _return_button_5
+
+_handle_button_7:
+	
+	lsr r4, r4, #8 // Moving output vector into the first 8 bit of the register
+
+  	mov r8, #0xff // Mask for extracting only the bits of the output vector
+
+	and r4, r4, r8 // Does extraction
+
+	mov r8, #0x80 // Mask for extracting bit 7 in the output vector
+
+	and r8, r8, r4 // Does extraction
+
+	lsl r4, r4, #9 // Moving the output vector back to the output window shifted one bit to the left
+
+	lsl r8, r8, #1 // Movong the first bit of the output vector to last position 
+
+  	orr r4, r4, r8 // Adding the last bit to the output word
+	
+	B _return_button_7	
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
