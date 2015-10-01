60d87055310ad64e3a0d67b5bd5e791c8274ddb4
Eivind Fylkesnes <eivindfy@its-015-12.idi.ntnu.no>
2015-10-01 14:53:16 +0200

    gpio enable

--- exercise2/ex2_support/gpio.c
+++ exercise2/ex2_support/gpio.c
@@ -7,15 +7,14 @@
 void setupGPIO()
 {
   /* TODO set input and output pins for the joystick */
-    
+  *CMU_HFPERCLKEN0 |= (1 << 13);
+
+  *GPIO_PC_MODEL |= 0x33333333;	
+  *GPIO_PC_DOUT |= 0xff;	
+
+  *GPIO_PA_CTRL |= 0x2;
+  *GPIO_PA_MODEH |= 0x55555555;
   /* Example of HW access from C code: turn on joystick LEDs D4-D8
      check efm32gg.h for other useful register definitions
   */
-  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO; /* enable GPIO clock*/
-  *GPIO_PA_CTRL = 2;  /* set high drive strength */
-  *GPIO_PA_MODEH = 0x55555555; /* set pins A8-15 as output */
-  *GPIO_PA_DOUT = 0x0700; /* turn on LEDs D4-D8 (LEDs are active low) */
 }
-
-
-
