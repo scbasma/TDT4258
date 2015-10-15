5a2bda9aa914cd057565b35430d7bf8df008d1d8
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-10-15 18:35:28 +0200

    flaaaklypa functioning

--- exercise2/ex2_support/interrupt_handlers.c
+++ exercise2/ex2_support/interrupt_handlers.c
@@ -2,13 +2,14 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
+#include "sound.h"
 
 int count = 0;
 int amplitude = 2000;
 int freq = 440;
-int sampleFreq = 20000;
-double freqFactor = double(freq)/double(sampleFreq);
-
+const int sampleFreq = 20000;
+int scala_counter = 0;
+int note_counter = 0;
 
 /* TIMER1 interrupt handler */
 void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
@@ -16,12 +17,25 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
   /*
     TODO feed new samples to the DAC
     remember to clear the pending interrupt by writing 1 to TIMER1_IFC
-  */ 
+  */
+  int note_frequency = scala[scala_counter]; 
+  double freqFactor = (double)note_frequency/(double)sampleFreq;
   *TIMER1_IFC |= 0x1;
 
-	*DAC0_CH0DATA = count*amp*freqFactor;
-	
-  if( count == sampleFreq/freq){
+  *DAC0_CH0DATA = (int) count*amplitude*freqFactor;
+  
+
+  if(note_counter == 5000){
+	note_counter = 0;
+	count = 0;
+	scala_counter += 1;
+	if (scala_counter > 31)
+		scala_counter = 0;
+   }else {
+	note_counter += 1;
+   }	
+
+  if( count == sampleFreq/note_frequency){
 		count = 0;
   }else{
 		count++;
--- exercise2/ex2_support/timer.c
+++ exercise2/ex2_support/timer.c
@@ -17,8 +17,7 @@ void setupTimer(uint16_t period)
     This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
   */  
   *CMU_HFPERCLKEN0 |= (0x1 << 6);
-  *TIMER1_TOP |= 0x5dc0;
-	*TIMER1_BASE &= ~(0xf << 24);
+  *TIMER1_TOP = 350;
   *TIMER1_IEN |= 0x1;
   *TIMER1_CMD |= 0x1;
 }
